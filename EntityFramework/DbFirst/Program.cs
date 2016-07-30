d7211b55e698824c5de8d348ce57d1ba9aa2a4b6
Luis Fernando Forero G. <foluis@hotmail.com>
2016-07-30 09:42:30 -0500

    Commit inicial

---
+++ EntityFramework/DbFirst/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbContext = new PlutoDbContext();
+            var courses = dbContext.GetCourses();
+            foreach (var course in courses)
+            {
+                Console.WriteLine(course.Title);
+            }
+            Console.ReadLine();
+        }
+    }
+}
